ef8c92c7415e306c5a568bf091fa92298c581530
pvKuznecov <p.v.kuznecov@yandex.ru>
2022-10-10 16:09:22 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,37 @@
-"use strict";
+$(document).ready(function(){
+  "use strict";
 
-const greatingName = "Доброго времени суток.";
+  // function showSContent(textId, textContentid) {
+  //   let elem = document.querySelector(textId);
+  //   if ($(textName).is(":hidden")) {
+  //           $(textContent).show("slow");
+  //       } else {
+  //           $(textContent).hide("slow");
+  //       }
+  //       return false;
+  //   };
+  //
+  $(".section_contentName").click(function () {
+    let elem = document.querySelector(".section_contentName");
+    let thisElem = $(this);
+    let parent = elem.closest('.section_content');
+    let parentId = parent.id;
+    console.log("elem::" + elem);
+    console.log("AAAAAAA" + this.value);
+    console.log("2" + thisElem.currentTarget);
+    console.log("thisElem::" + thisElem);
+    console.log(thisElem.id);
+    console.log(parent);
+    console.log(parentId);
+    console.log("____________________");
 
-// alert(greatingName);
+    if ($(".section_contentText").is(":hidden")) {
+            $(".section_contentText").show("slow");
+        } else {
+            $(".section_contentText").hide("slow");
+        }
+        return false;
+    });
+
+
+});

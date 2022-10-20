52c3c0a70fa6266657d9bf55cbd1e0ef8099b18d
pvKuznecov <p.v.kuznecov@yandex.ru>
2022-10-20 17:52:25 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
   $(".nav_showAction").click(function () {
     if ($(".nav_hName").is(":hidden")) {
       $(".nav_hName").show();
-      $(".main_navigator").width("7%");
+      $(".main_navigator").width("9%");
     } else {
       $(".nav_hName").hide();
       $(".main_navigator").width("22px");

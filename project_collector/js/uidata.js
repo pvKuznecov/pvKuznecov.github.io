f231622df4becdcc89047cc46791e6bc9e4f836a
pvKuznecov <p.v.kuznecov@yandex.ru>
2022-10-28 17:30:24 +0700

    Add files via upload

---
+++ project_collector/js/uidata.js
@@ -0,0 +1,9 @@
+const ScnData = {textName: "Главная страница", scnLinkName: "main_page"};
+const UiData = {
+  navigat: {
+    main: [
+      {id: 0, textName: "Главная страница", scnLinkName: "main_page"},
+      {id: 1, textName: "История", scnLinkName: "story_page"},
+    ],
+  },
+};

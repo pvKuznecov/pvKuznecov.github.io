63ccb7aeb2fa859d38dcf911aa093222982723f7
pvKuznecov <p.v.kuznecov@yandex.ru>
2022-10-28 17:29:58 +0700

    Update ui.js

--- project_collector/js/ui.js
+++ project_collector/js/ui.js
@@ -1,82 +1,94 @@
 $(document).ready(function(){
   "use strict";
 
-const HMaker = {
-  newButton: function(mother, className, idName) {
+  const WorkSpace = {
+    scnLinkName: "",
+    textName: "",
+  };
+  const MainNavigator = addNavigat();
+  // при первом входе - запускаем стартер
+  if (WorkSpace.scnLinkName == "") {
+    getStart();
+  };
+
+  function getStart() {
+    // наполняем начальными данными рабочее пространство
+    WorkSpace.scnLinkName = ScnData.scnLinkName;
+    WorkSpace.textName = ScnData.textName;
+    // наполняем главный навигатор кнопками
+    for (let i = 0; i < UiData.navigat.main.length; i++) {
+      let selectedVal = UiData.navigat.main[i];
+      navButMaker(MainNavigator, "mNavig_button", ("nav-" + selectedVal.scnLinkName), selectedVal.textName, selectedVal.scnLinkName);
+    };
+  };
+
+  // генератор кнопок: ("мать", "наименование класса", "наименование id", "текстовое содержимое", "scn")
+  function navButMaker(mother, className, idName, textContent, scnVal) {
     let child = document.createElement('button');
-    console.log("className: " + className);
+    if (typeof idName == "string") {
+      child.id = idName;
+    };
+    if (typeof className == "string") {
+      child.className = className;
+    };
+    if (typeof textContent == "string") {
+      child.textContent = textContent;
+    };
+    if (typeof scnVal == "string") {
+      child.onclick = () => mainWindow_generator(scnVal, textContent);
+    };
     mother.appendChild(child);
     return child;
-  },
-};
+  };
 
-const MainNavigator = addNavigat();
-function addNavigat() {
-  let target = document.getElementById('mainNav');
-  let navigat = document.createElement('div');
-  navigat.id = "mainNavigator";
-  target.appendChild(navigat);
-  return navigat;
-};
+  // создание поля под навигатор боковой панели
+  function addNavigat() {
+    let target = document.getElementById('mainNav');
+    let navigat = document.createElement('div');
+    navigat.id = "mainNavigator";
+    target.appendChild(navigat);
+    return navigat;
+  };
 
-console.log(MainNavigator);
-console.log(typeof MainNavigator);
-HMaker.newButton(MainNavigator);
-console.log(MainNavigator);
-console.log(typeof MainNavigator);
+  // строитель главного окна
+  function mainWindow_generator(scnLinkName, txtVal) {
+    let scn = scnLinkName;
+    if (WorkSpace.scnLinkName == scn) {
+      return;
+    };
+    WorkSpace.scnLinkName = scnLinkName;
+    WorkSpace.textName = txtVal;
 
-// наполнение блоками со статьями из вк______________________________________
-function makeContentCube(vkArticle, uName) {
-  let divValue = document.createElement('div');
-  let target = document.getElementById('mainWindow');
-  divValue.id = ("vk_article_-" + vkArticle);
-  target.appendChild(divValue);
-  VK.Widgets.Article(("vk_article_-" + vkArticle), ("@-" + uName));
-  divValue.style = null;
-  divValue.className = "contentCube";
-};
+    let mWindow = document.getElementById('mainWindow');
+    while (mWindow.firstChild) {
+      mWindow.removeChild(mWindow.firstChild);
+    };
+    switch (scn) {
+      case "main_page":
+        break;
+      case "story_page":
+        let vkPosts = ContentData.vk.posts;
+        for (var i = 0; i < vkPosts.length; i++) {
+          makeContentCube(vkPosts[i].vk_article, vkPosts[i].uName);
+        };
+        break;
+      default:
+        break;
+    };
+  };
 
-let vkPosts = ContentData.vk.posts;
-for (var i = 0; i < vkPosts.length; i++) {
-  console.log(vkPosts[i]);
-  makeContentCube(vkPosts[i].vk_article, vkPosts[i].uName);
-};
-// ____________________________________________________________________________
 
-// console.log(typeof document.body.childNodes);
-//
-// let mainWindow = document.getElementById('mainWindow');
-// console.log(mainWindow);
-//
-//
-// let bodyVal = document.querySelector('body');
-// let bodyData = bodyVal.childNodes;
-//
-// let a = document.getElementById("mainHeader");
-// console.log(a);
-// console.log(document.getElementById("mainHeader"));
-// _______________________________________________________________________
 
-// // console.log(bodyData);
-// // console.log(bodyData.length);
-// // // console.log(bodyData[0]);
-// // // console.log(bodyData[1]);
-// // console.log(bodyData.textContent);
-// let elementNode = document.createElement('b');
-// elementNode.textContent = " мир!";
-// // let textNode = document.createTextNode(' мир!');
-// console.log(elementNode);
-// // console.log(textNode);
-// //добавим эти узлы к DOM
-// // document.querySelector('p').appendChild(elementNode);
-// document.querySelector("p").insertBefore(elementNode, document.querySelector("p").firstElementChild);
-// // document.querySelector('b').appendChild(textNode);
-//
-// let primList = ["а", "б", "в"];
-// for (var i = 0; i < primList.length; i++) {
-//   // let x = document.createTextNode(primList[i]);
-//   // document.querySelector('p').appendChild(x);
-//   // document.querySelector('p').appendChild('br');
-//
-// };
+  // наполнение блоками со статьями из вк______________________________________
+  function makeContentCube(vkArticle, uName) {
+    let divValue = document.createElement('div');
+    let target = document.getElementById('mainWindow');
+    divValue.id = ("vk_article_-" + vkArticle);
+    target.appendChild(divValue);
+    VK.Widgets.Article(("vk_article_-" + vkArticle), ("@-" + uName));
+    divValue.style = null;
+    divValue.className = "contentCube";
+  };
+
+  mainWindow_generator(WorkSpace.scnLinkName, "Главная страница");
 });

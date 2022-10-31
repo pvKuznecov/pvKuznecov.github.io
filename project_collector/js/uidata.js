8b170c9afc07020e598c7a11e3b85f2a384e010e
pvKuznecov <p.v.kuznecov@yandex.ru>
2022-10-31 16:55:26 +0700

    Update uidata.js

--- project_collector/js/uidata.js
+++ project_collector/js/uidata.js
@@ -1,9 +1,16 @@
 const ScnData = {textName: "Главная страница", scnLinkName: "main_page"};
+const TopBarButtons = [
+  {id: 0, strName: "О проекте", subScnName: "about", scnPrefix: "topBar"},
+  {id: 1, strName: "Помощник", subScnName: "helper", scnPrefix: "topBar"}
+];
 const UiData = {
   navigat: {
     main: [
-      {id: 0, textName: "Главная страница", scnLinkName: "main_page"},
-      {id: 1, textName: "История", scnLinkName: "story_page"},
+      {id: 0, textName: "Главная страница", scnLinkName: "main_page", topBarId: [0, 1,]},
+      {id: 1, textName: "История", scnLinkName: "story_page", topBarId: [0,]},
+    ],
+    links: [
+      {id: 0, textName: "ВК", linkStr: "https://vk.com/public185014859"},
     ],
   },
 };

ec1615cc11322c3a6ef09bf686aa39b10056bfa8
pvKuznecov <p.v.kuznecov@yandex.ru>
2022-11-25 12:08:22 +0700

    Update ui.js

--- project_collector/js/ui.js
+++ project_collector/js/ui.js
@@ -8,11 +8,86 @@ $(document).ready(function(){
   const MainNavigator = addNavigat();
   const MainFooter = document.getElementById('mainFooter');
   const TopBar = document.getElementById('mainHeader');
-  // при первом входе - запускаем стартер
-  if (WorkSpace.scnLinkName == "") {
-    getStart();
+  const MainWindowObj = {
+    main_page: function() {
+      console.log("mainWindow_generator: main_page");
+      let target = document.getElementById('mainWindow');
+      let subWind_main = new AddElem.div("subWind", "subWind_main_page");
+      let titleH2Text = document.createElement('h2');
+      titleH2Text.textContent = "Главная страница";
+      subWind_main.appendChild(titleH2Text);
+      target.appendChild(subWind_main);
+      return;
+    },
+    story_page: function() {
+      console.log("mainWindow_generator: story_page");
+      let target = document.getElementById('mainWindow');
+      let subWind_storyPage = new AddElem.div("subWind", "subWind_story_page");
+      let titleH2Text = new AddElem.h2();
+      titleH2Text.textContent = "История";
+
+      let mainText = new AddElem.p();
+      mainText.textContent = "Текст, который должен быть здесь. Просто потому..."
+
+      target.appendChild(titleH2Text);
+      target.appendChild(mainText);
+      target.appendChild(subWind_storyPage);
+      let vkPosts = ContentData.vk.posts;
+      for (var i = 0; i < vkPosts.length; i++) {
+        makeContentCube("subWind_story_page", vkPosts[i].vk_article, vkPosts[i].uName);
+      };
+    },
+    wikibook: function() {
+      console.log("mainWindow_generator: wikibook");
+      let target = document.getElementById('mainWindow');
+      let subWind_wiki = new AddElem.div("subWind", "subWind_wikibook");
+      let titleH2Text = document.createElement('h2');
+      titleH2Text.textContent = "«Wiki-Book»";
+      subWind_wiki.appendChild(titleH2Text);
+      target.appendChild(subWind_wiki);
+    },
+    all_content: function() {
+      console.log("mainWindow_generator: all_content");
+      let target = document.getElementById('mainWindow');
+      let subWind_allContent = new AddElem.div("subWind", "subWind_all_content");
+      let titleH2Text = document.createElement('h2');
+      titleH2Text.textContent = "Все статьи";
+      subWind_allContent.appendChild(titleH2Text);
+      target.appendChild(subWind_allContent);
+    },
+    my_games: function() {
+      console.log("mainWindow_generator: my_games");
+      let target = document.getElementById('mainWindow');
+      let subWind_myGames = new AddElem.div("subWind", "subWind_my_games");
+      let titleH2Text = document.createElement('h2');
+      titleH2Text.textContent = "«Разбор полетов»";
+      subWind_myGames.appendChild(titleH2Text);
+      target.appendChild(subWind_myGames);
+    },
   };
 
+  let AddElem = {
+    p: function() {
+      let res = document.createElement('p');
+      return res;
+    },
+    h2: function() {
+      let res = document.createElement('h2');
+      return res;
+    },
+    div: function(className, idVal) {
+      let result = document.createElement('div');
+      if (typeof className == "string") {
+        result.className = className;
+      };
+      if (typeof idVal == "string") {
+        result.id = idVal;
+      };
+      return result;
+    }
+  };
+
+  // первый запуск
   function getStart() {
     // наполняем начальными данными рабочее пространство
     WorkSpace.scnLinkName = ScnData.scnLinkName;
@@ -50,6 +125,12 @@ $(document).ready(function(){
       let selectedVal = UiData.navigat.links[i];
       new NavButMaker(MainFooter, "mFooter_button", ("footer-" + selectedVal.id), selectedVal.textName, null, selectedVal.linkStr);
     };
+    MainWindowObj.main_page();
+  };
+
+  // при первом входе - запускаем стартер
+  if (WorkSpace.scnLinkName == "") {
+    getStart();
   };
 
   // генератор кнопок: ("мать", "наименование класса", "наименование id", "текстовое содержимое", "scn", "ссылка на внешний источник")
@@ -123,16 +204,23 @@ $(document).ready(function(){
         new NavButMaker(TopBar, "mainHeader_topButton", ("topBar_" + topBarIdList[i]), butText, null, null);
       };
     };
-
-
+    let target = document.getElementById('mainWindow');
+    // создаем рабочую область
     switch (scn) {
-      case "main_page":
+      case "main_page": //"Главная страница"
+        MainWindowObj.main_page();
         break;
-      case "story_page":
-        let vkPosts = ContentData.vk.posts;
-        for (var i = 0; i < vkPosts.length; i++) {
-          makeContentCube(vkPosts[i].vk_article, vkPosts[i].uName, vkPosts[i].tags);
-        };
+      case "story_page":  //"История"
+        MainWindowObj.story_page();
+        break;
+      case "wikibook":  //"«Wiki-Book»"
+        MainWindowObj.wikibook();
+        break;
+      case "all_content": //"Все статьи"
+        MainWindowObj.all_content();
+        break;
+      case "my_games":  //"«Разбор полетов»"
+        MainWindowObj.my_games();
         break;
       default:
         break;
@@ -142,14 +230,20 @@ $(document).ready(function(){
 
 
   // наполнение блоками со статьями из вк______________________________________
-  function makeContentCube(vkArticle, uName, tagsList) {
+  function makeContentCube(parrentTarget, vkArticle, uName) {
+    let target = document.getElementById(parrentTarget);
+    console.log("target:: ");
+    console.log(target);
     let divValue = document.createElement('div');
-    let target = document.getElementById('mainWindow');
-    divValue.id = ("vk_article_-" + vkArticle);
-    target.appendChild(divValue);
-    VK.Widgets.Article(("vk_article_-" + vkArticle), ("@-" + uName));
-    divValue.style = null;
+    let innDivValue = document.createElement('div');
     divValue.className = "contentCube";
+    divValue.style = "";
+    innDivValue.id = ("vk_article_-" + vkArticle);
+    VK.Widgets.Article(("vk_article_-" + vkArticle), ("@-" + uName));
+    divValue.appendChild(innDivValue);
+
+
+    target.appendChild(divValue);
   };
 
   // копирование объекта (дубликат, не ссылка)_________________________________
@@ -161,5 +255,50 @@ $(document).ready(function(){
     return newObj;
   };
 
-  mainWindow_generator(WorkSpace.scnLinkName, "Главная страница");
+  // сортировка чисел
+  function numbSort(a, b) {
+    return a-b;
+  };
+
+  // let atest = [1, 2, 3];
+  // let btest = [3, 8, 4, 5, 1];
+  // let restest = [];
+  // console.log("atest: " + atest);
+  // console.log("btest: " + btest);
+  // console.log("restest: " + restest);
+  // restest = restest.concat(atest, btest);
+  // console.log("restest: " + restest);
+  // console.log(restest.indexOf(3, 0));
+  //
+  // console.log("sort atest: " + atest.sort(numbSort));
+  // console.log("sort btest: " + btest.sort(numbSort));
+  // console.log("sort restest: " + restest.sort(numbSort));
+
+  // Метод sort можно удобно использовать с функциональными выражениями (и замыканиями):
+  // var numbers = [4, 2, 5, 1, 3];
+  // numbers.sort(function(a, b) {
+  //   return a - b;
+  // });
+  // console.log(numbers); // [1, 2, 3, 4, 5]
+  // ---------------------------------------------------
+  // Объекты могут быть отсортированы по значению одного из своих свойств.
+  // var items = [
+  //   { name: 'Edward', value: 21 },
+  //   { name: 'Sharpe', value: 37 },
+  //   { name: 'And', value: 45 },
+  //   { name: 'The', value: -12 },
+  //   { name: 'Magnetic' },
+  //   { name: 'Zeros', value: 37 }
+  // ];
+  // items.sort(function (a, b) {
+  //   if (a.name > b.name) {
+  //     return 1;
+  //   }
+  //   if (a.name < b.name) {
+  //     return -1;
+  //   }
+  //   // a должно быть равным b
+  //   return 0;
+  // });
+
 });
